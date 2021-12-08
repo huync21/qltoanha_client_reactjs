001b6fcecb1f3e48d8f01ec381525a802355d994
huy <huync21@gmail.com>
2021-12-08 18:03:10 +0700

    fix conflict

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -4,13 +4,15 @@ import floorReducers from './floor';
 import loginReducers from './login';
 import registeredServiceReducers from './registered_service';
 import rentedAreaReducers from './rented_area'
+import serviceReducers from './service';
+
 const reducers = combineReducers({
     login: loginReducers,
     company: companyReducers,
     registeredService: registeredServiceReducers,
-    
     floors: floorReducers,
     rentedAreas: rentedAreaReducers
+    service: serviceReducers
 });
 
-export default (state, action) => reducers(state, action);
+export default (state, action) => reducers(state, action);  
