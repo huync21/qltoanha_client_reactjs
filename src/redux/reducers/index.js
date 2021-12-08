50520de8a90e9e44c3c5ffdda09f4740777778ee
Dumb Cate <daokynguyen2000@gmail.com>
2021-12-08 17:59:54 +0700

    update-index.js-file

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,13 +1,17 @@
 import {combineReducers} from 'redux';
 import companyReducers from './company';
+import floorReducers from './floor';
 import loginReducers from './login';
 import registeredServiceReducers from './registered_service';
+import rentedAreaReducers from './rented_area'
 import serviceReducers from './service';
 
 const reducers = combineReducers({
     login: loginReducers,
     company: companyReducers,
     registeredService: registeredServiceReducers,
+    floors: floorReducers,
+    rentedAreas: rentedAreaReducers,
     service: serviceReducers
 });
 
